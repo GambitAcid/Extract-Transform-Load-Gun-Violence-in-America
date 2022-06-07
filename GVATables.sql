4a737cdb680631c9cb6fea7241b42ee42affa753
LAFF1 <98897041+LAFF1@users.noreply.github.com>
2022-06-07 13:08:17 -0400

    Create GVATables.sql

---
+++ GVATables.sql
@@ -0,0 +1,37 @@
+﻿-- Basic table layout for GVA source data 
+
+CREATE TABLE "Incident" (
+    "IncidentID" int  NOT NULL,
+    "Date" date   NOT NULL,
+    "State" varchar(20)  NOT NULL,
+    "Jurisiction" varchar(20),
+    "Address" (20),
+    "No_Killed" int,
+    "No_injured" int,
+    "IncidentUrl" varchar(50),
+    "SourceUrl" varchar(50), 
+    "House_District" int,
+    "Senate_District" int,
+    CONSTRAINT "pk_Incident" PRIMARY KEY (
+        "IncidentID"
+     )
+);
+
+CREATE TABLE "Participants" (
+    "ParticipantID" SERIAL,
+    "IncidentID" int not NULL,
+    "Age_group" varchar(10),
+    "Gender" varchar(10),
+    "Name" varchar(30),
+    "Status" varchar(20),
+    "Type" varchar(20)
+    CONSTRAINT "pk_Participants" PRIMARY KEY (
+        "ParticipantID"
+     )
+);
+
+
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_IncidentID" FOREIGN KEY("IncidentID")
+REFERENCES "Participants" ("IncidentID");
+
